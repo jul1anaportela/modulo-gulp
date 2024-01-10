5493dd810ca990ba2c5454eb774ebedb287b5a92
jul1anaportela <jul1anaportela@outlook.com>
2024-01-09 21:19:07 -0300

    Atividade M16 - Gulp - EBAC :tada:

---
+++ source/scripts/aritmetica.js
@@ -0,0 +1,9 @@
+const somar = require('./sum')
+const subtrair = require ('./takeaway')
+const multiplicar = require ('./multiply')
+const dividir = require('./split')
+
+console.log(somar(10,20))
+console.log(subtrair(40,35))
+console.log(multiplicar(9,8))
+console.log(dividir(10,2))
